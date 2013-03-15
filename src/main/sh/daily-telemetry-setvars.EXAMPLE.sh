bd5accd6e15783f21019d73d405499f2a2c5508e
Mark Reid <mreid@mozilla.com>
2013-03-15 14:51:39 -0300

    Add a date parameter (for backprocessing).

--- src/main/sh/daily-telemetry-setvars.EXAMPLE.sh
+++ src/main/sh/daily-telemetry-setvars.EXAMPLE.sh
@@ -5,8 +5,17 @@ PIG_HOME=/usr/lib/pig
 ETL_HOME=/home/etl
 ES_CONFIG=$ETL_HOME/elasticsearch/elasticsearch.yml
 ES_PLUGINS=$ETL_HOME/elasticsearch/plugins
+
+## Default to running for yesterday.
 YESTERDAY="`date +%Y%m%d --date="1 day ago"`"
 YESTERDAY_YYYY_MM="`date +%Y%m --date="1 day ago"`"
+
+## If a parameter was passed in, use that for the target day:
+if [ ! -z "$1" ]; then
+  YESTERDAY=$(date -d "$1" +%Y%m%d)
+  YESTERDAY_YYYY_MM=$(date -d "$1" +%Y%m)
+fi
+
 VALID_INDEX_NAME="telemetry_agg_valid_$YESTERDAY_YYYY_MM"
 PUB_SERVER=example.mozilla.com
 HDFS_EXPORT_PATH=hdfs://example.mozilla.com:8020/user/$USER
